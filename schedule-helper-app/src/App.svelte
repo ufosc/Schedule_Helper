d9c1eff90028e82193ac1674ea98998da73f3772
Robert Conde <rconde2003@gmail.com>
2023-01-09 15:50:12 -0500

    Issue
    Look at the console.
    addSelection function is apparently not define... -- "Uncaught ReferenceError: addSelection is not defined at HTMLButtonElement.onclick".

--- schedule-helper-app/src/App.svelte
+++ schedule-helper-app/src/App.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
     import DisplaySections from "./components/DisplaySections.svelte";
+    import Selections from "./components/Selections.svelte";
 </script>
 
+<body>
 <main class="w-screen h-screen">
     <p class="m-4 text-2xl text-slate-700">Schedule Helper 📆</p>
 
+    <Selections/>
+
     <DisplaySections/>
 </main>
+</body>
---
+++ schedule-helper-app/src/components/Selections.svelte
@@ -0,0 +1,12 @@
+<script>
+    function addSelection(addSelectionButton) {
+        let courseSelections = addSelectionButton.parent.querySelector("#add-selection");
+        console.log(courseSelections);
+    }
+</script>
+
+<div class="inline-block p-2 ml-2 bg-slate-100">
+    <div id="course-selections"></div>
+
+    <button id="add-selection" onclick="addSelection(this)">Add a Selection</button>
+</div>
